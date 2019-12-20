57002b65f6b1827ec39bc6df5e5392cc28a3a6e2
callmegoon <matt@leveragenam.com>
2019-12-20 11:26:17 -0600

    updated to new IT branding;

--- source/library/javascript/global.js
+++ source/library/javascript/global.js
@@ -1,77 +1,46 @@
-/****************************** WEB STANDARD POP UPS ******************************/
-  function strictNewWindow() {
-    if (!document.getElementsByTagName("a")) {
-      return false;
-    }
-    $("a.newwindow").click(function() {
-      window.open($(this).attr("href"));
-      return false;
-    });
-  }
+function mobileNav() {
+  var hamburger = document.querySelector('.hamburger');
+  var body = document.querySelector('body');
 
-/****************************** HAMBURGER ******************************/
-  function navHmaburger() {
-    $('.hamburger').on('click tap', function(){
-      $('.hamburger').toggleClass('open');
-      $('.main-nav').toggleClass('active');
-      $('body').toggleClass('no-scroll');
-      $('html').toggleClass('no-scroll');
-    });
-  }
-
-/****************************** STICKY NAV ******************************/
-  function stickyHeader() {
-    var rafTimer;
-    var header = $(".main-header");
-
-    function toggleStickyHeader() {
-      // does cause layout/reflow: https://git.io/vQCMn
-      if (window.scrollY > 100) {
-        header.addClass('sticky');
-      } else {
-        header.removeClass('sticky');
-      }
-    }
-
-    window.onscroll = function () {
-      cancelAnimationFrame(rafTimer);
-      rafTimer = requestAnimationFrame(toggleStickyHeader);
-    };
-  }
-
-/****************************** BRAND SLIDER ******************************/
-  var elem = document.querySelector(".slider-brands");
-  var flkty = new Flickity(elem, { 
-    // options
-    cellAlign: 'left',
-    wrapAround: true,
-    pageDots: false,
-    imagesLoaded: true,
-    autoPlay: 5000,
-    arrowShape: { 
-      x0: 10,
-      x1: 50, y1: 50,
-      x2: 50, y2: 10,
-      x3: 50
+  hamburger.addEventListener('click', function () {
+    hamburger.classList.toggle('is-active');
+    if (body.classList.contains('navOpen')) {
+      body.classList.remove('navOpen');
+    } else {
+      body.classList.add('navOpen');
     }
   });
+}
 
-/****************************** MOBILE FOOTER LINKS ******************************/
+function stickyNav() {
+  // var windowHeight = window.innerHeight;
+  // alert(windowHeight);
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 200) {
+      $("body").addClass("scrolled");
+    } else {
+      $("body").removeClass("scrolled");
+    }
+  });
+}
+
+function footerToggles() {
   if (window.innerWidth < 960) {
-    $('.link-list').hide();
-    $('.col--title').addClass('inactive');
-    $('.col--title').on('click tap', function() {
-      $(this).toggleClass('inactive');
-      $(this).parent().toggleClass('open');
-      $(this).parent().find('.link-list').slideToggle();
-    });
+    $('.linkLists__list').hide();
   }
 
-//************************************************************************************************************************//
-//************************************************** LOAD ALL FUNCTIONS **************************************************//
-//************************************************************************************************************************//
-$(document).ready(function() {
-  strictNewWindow();
-  navHmaburger();
-  stickyHeader();
+  $('.linkLists__colTitle').on('click tap', function () {
+    $(this).parent().toggleClass('active');
+    $(this).parent().find('.linkLists__list').slideToggle();
+  });
+}
+
+
+
+
+// Load 'em
+document.addEventListener('DOMContentLoaded', function () {
+  mobileNav();
+  stickyNav();
+  footerToggles();
 });
